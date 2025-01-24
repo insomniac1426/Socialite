d58760fbb29b5e39f82fa08ecb02ce3a4fc2f618
Bugbuster2004 <siddhant6338bansal@gmail.com>
2025-01-24 19:04:42 +0530

    finalised

--- backend/src/index.js
+++ backend/src/index.js
@@ -29,12 +29,13 @@ app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+  app.use(express.static(path.join(__dirname, "frontend/vite-project/dist")));
 
   app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+    res.sendFile(path.join(__dirname, "frontend/vite-project/dist", "index.html"));
   });
 }
+
 
 server.listen(PORT, () => {
   console.log("server is running on PORT:" + PORT);
